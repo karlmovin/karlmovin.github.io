4ac5e36846ad0a362bfcd3b4ed1400787aee082c
Karl <40166249+kfmovin@users.noreply.github.com>
2024-03-26 22:08:28 +0100

    update list visuals

--- src/routes/packning.tsx
+++ src/routes/packning.tsx
@@ -536,23 +536,66 @@ export default function Packlistor() {
         .map(({ title, data }) => (
           <section id={title} key={title} className="flex flex-col">
             <p className="text-4xl self-center pb-3">{title}</p>
-            <div className="mt-2 ml-1 flex flex-row flex-wrap">
+            <div className="mt-2 gap-2 flex flex-row flex-wrap">
               {data.map(({ title: dataTitle, subtitle, items }) => (
-                <div className="max-w-1/2 min-w-56 pl-3">
-                  <p className="text-xl">{dataTitle}</p>
-                  <p className="text-sm py-2 whitespace-pre-line">{subtitle}</p>
-                  <ol className="">
-                    {items?.map((item) => (
-                      <li key={item}>
-                        <input
-                          type="checkbox"
-                          className="checked:bg-blue-500 mr-1"
-                        />
-                        {item}
-                      </li>
-                    ))}
-                  </ol>
-                  <br />
+                <div className="flex flex-col">
+                  <p className="text-xl self-center font-medium">{dataTitle}</p>
+                  {subtitle && (
+                    <p className="text-sm py-2 whitespace-pre-line">
+                      {subtitle}
+                    </p>
+                  )}
+                  {items && (
+                    <div className="relative flex flex-col text-gray-700 bg-white shadow-md rounded-b-xl bg-clip-border">
+                      <div className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700 divide-y">
+                        {items?.map((item) => (
+                          <div
+                            role="button"
+                            className="flex items-center w-full p-0 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+                          >
+                            <label
+                              htmlFor={`vertical-list-${item}`}
+                              className="flex items-center w-full px-3 py-2 cursor-pointer"
+                            >
+                              <div className="grid mr-3 place-items-center">
+                                <div className="inline-flex items-center">
+                                  <label
+                                    className="relative flex items-center p-0 rounded-full cursor-pointer"
+                                    htmlFor={`vertical-list-${item}`}
+                                  >
+                                    <input
+                                      id={`vertical-list-${item}`}
+                                      type="checkbox"
+                                      className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border-2 border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
+                                    />
+                                    <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-3.5 w-3.5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        stroke="currentColor"
+                                        stroke-width="1"
+                                      >
+                                        <path
+                                          fill-rule="evenodd"
+                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                          clip-rule="evenodd"
+                                        ></path>
+                                      </svg>
+                                    </span>
+                                  </label>
+                                </div>
+                              </div>
+                              <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                                {item}
+                              </p>
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  )}
                 </div>
               ))}
             </div>
