e7a6a59b64fbd13c429990e2306464e74d9dc61c
Karl <40166249+kfmovin@users.noreply.github.com>
2024-04-07 21:41:17 +0200

    fler todos

--- src/routes/todos.tsx
+++ src/routes/todos.tsx
@@ -3,6 +3,11 @@ const todos = [
   "känsla->behov->vilja->handling",
   "kika på https://github.com/remarkjs/react-markdown?tab=readme-ov-file",
   "inkludera rimliga tider i vad ska jag göra?",
+  "fundera över hur typisk konst för en period ska visas på konstsidan",
+  "fundera över hur bokmärkena ska sorteras på ett vettigt sätt",
+  "flytta väderdatan åt sidan",
+  "använd metAPIs ikoner, fundera över att använda existerande lib för detta",
+  "vis förslagen på vad jag kan göra på ett bättre sätt",
 ];
 
 export default function Todos() {
