0ba9b542593c1b39243f749feab1f0ea18139c52
Karl <40166249+kfmovin@users.noreply.github.com>
2024-04-26 20:51:12 +0200

    move links

--- src/routes/todos.tsx
+++ src/routes/todos.tsx
@@ -7,13 +7,7 @@ const todos = [
   "flytta väderdatan åt sidan",
   "använd metAPIs ikoner, fundera över att använda existerande lib för detta",
   "visa förslagen på vad jag kan göra på ett bättre sätt",
-  "kika på https://texttv.nu/blogg/texttv-api",
   "fler gubbar, minst 4. 2 skarvsladdar a 10 m. 1 vinda. 2 polermaskiner. 4 stegar , möjligen fixade som en ställning med gåplank. många rena microfleecedukar. tvål för rengöring. kläder som kan bli skitiga. torrt uppe vid relingen, dvs avtorkat efter tvätten. högtalare. fika. lunch. papper för applicering av medel. hushållspapper o handdukar. process: applicera med papper, polera med maskin. krokar gör att hänga saker på. örpnproppar. visir. ansiktsskydd.",
-  "https://www.trafiklab.se/sv/api/trafiklab-apis/sl/",
-  "https://api.sl.se/fordonspositioner#PT",
-  "https://en.parkopedia.se/",
-  "https://loppistajm.se/kalender.html",
-  "https://ground.news/landingV7/nutshell?utm_source=nutshell&utm_medium=Youtube&utm_campaign=nov30",
 ];
 
 export default function Todos() {
