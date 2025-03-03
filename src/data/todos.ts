ae691aae84ec8b3259436670ee888b99608b9455
Karl <40166249+kfmovin@users.noreply.github.com>
2025-03-03 18:57:09 +0100

    refactor

---
+++ src/data/todos.ts
@@ -0,0 +1,4 @@
+export const todos = [
+  "kika på https://github.com/remarkjs/react-markdown?tab=readme-ov-file",
+  "Kika på https://twitter.com/i/events/994601867987619840 och https://medium.com/refactoring-ui/7-practical-tips-for-cheating-at-design-40c736799886, börja bygga temat...",
+];
