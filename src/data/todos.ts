23d2bb8209cf8af058cf40cfcbbcc7faba6d2aec
Karl <40166249+kfmovin@users.noreply.github.com>
2025-03-05 21:41:24 +0100

    add things

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -3,4 +3,5 @@ export const todos = [
   "try out https://d3js.org/",
   "search for code to diagram converter",
   "search for good design tool for layouts",
+  "does it exist an app for trying new things?",
 ];
