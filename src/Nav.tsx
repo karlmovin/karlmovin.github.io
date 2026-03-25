b5faf3577f4b878560340362feba44f912ebcc7d
Karl <karl@Karls-MacBook-Pro.local>
2026-03-25 22:09:10 +0100

    remove goals

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -113,18 +113,6 @@ export default function Nav() {
               >
                 Text-TV
               </NavLink>
-              <NavLink
-                to="../goals"
-                className={({ isActive }: { isActive: boolean }) =>
-                  `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
-                    isActive
-                      ? "border-gray-900 dark:border-white text-gray-900 dark:text-white"
-                      : "border-transparent text-gray-500 dark:text-gray-300 hover:border-gray-300 dark:hover:border-white hover:text-gray-700 dark:hover:text-white"
-                  }`
-                }
-              >
-                Mål
-              </NavLink>
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center sm:space-x-4">
@@ -267,18 +255,6 @@ export default function Nav() {
               }
             >
               Text-TV
-            </NavLink>
-            <NavLink
-              to="../goals"
-              className={({ isActive }: { isActive: boolean }) =>
-                `block pl-3 pr-4 py-2 border-l-4 text-base font-medium ${
-                  isActive
-                    ? "bg-gray-50 dark:bg-gray-800 border-gray-900 dark:border-white text-gray-900 dark:text-white"
-                    : "border-transparent text-gray-500 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-800 hover:border-gray-300 dark:hover:border-white hover:text-gray-700 dark:hover:text-white"
-                }`
-              }
-            >
-              Mål
             </NavLink>
             <button
               onClick={() => {
