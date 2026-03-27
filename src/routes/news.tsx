6a49b0812f2b5bc655d0a7bdb542682a370305fb
karlmovin <karlmovin@users.noreply.github.com>
2026-03-27 17:22:10 +0100

    change iframe src to texttv.nu

--- src/routes/news.tsx
+++ src/routes/news.tsx
@@ -1,8 +1,3 @@
 export default function News() {
-	return (
-		<iframe
-			className="w-full h-screen"
-			src="https://www.svt.se/text-tv/webb/100"
-		></iframe>
-	);
+	return <iframe className="w-full h-screen" src="https://texttv.nu/"></iframe>;
 }
