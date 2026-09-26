4ff6170fb5c4601437526600e0297416f0b608d0
karlmovin <karlmovin@users.noreply.github.com>
2026-09-26 19:05:28 +0200

    lägg till tyskland som land och ett vin från tyskland

--- src/data/wines.ts
+++ src/data/wines.ts
@@ -17,6 +17,7 @@ export type Wine = {
 
 export const countries = {
 	PT: { flag: "🇵🇹", name: { sv: "Portugal", en: "Portugal" } },
+	DE: { flag: "🇩🇪", name: { sv: "Tyskland", en: "Germany" } },
 } satisfies Record<string, Country>;
 
 export const wines: Wine[] = [
@@ -30,6 +31,18 @@ export const wines: Wine[] = [
 		notes: {
 			sv: "Gott sällskapsvin, passar till pasta och pizza.",
 			en: "Good social wine, pairs with pasta and pizza.",
+		},
+	},
+	{
+		name: "Peacock",
+		year: 2023,
+		rating: 4,
+		url: "https://www.systembolaget.se/produkt/vin/peacock-537201/",
+		country: countries.DE,
+		tags: ["vitt", "kerner", "rivaner", "asiatisk", "fisk", "sällskap", "sött"],
+		notes: {
+			sv: "Sött, småsurt sällskapsvin, passar till asiatisk mat och fisk.",
+			en: "Sweet, slightly sour social wine, pairs with Asian food and fish.",
 		},
 	},
 ];
